40e3cfe9808c951fdc45bdadbd194e3959a82614
Ed Finkler <coj@funkatron.com>
2020-02-02 22:21:24 -0500

    add player

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
+    // bind player buttons
+    const playerButtons = document.querySelectorAll('.bcplayer-container>svg');
+    for (const playerButton of playerButtons) {
+        playerButton.addEventListener('click', event => {
+            const container = playerButton.parentElement;
+            const bcplayer = container.querySelector('iframe');
+            playerButton.classList.toggle('hide');
+            bcplayer.classList.toggle('show');
         });
     }
 
 });
+
+
+
